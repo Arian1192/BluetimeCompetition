000fd393ec7b0d4b9e76f93a1002e39c976953b9
Arian1192 <Arian.collaso.rodriguez@gmail.com>
2023-01-07 00:35:48 +0100

    Modifico getStatiProps

--- pages/index.js
+++ pages/index.js
@@ -58,9 +58,16 @@ export async function getStaticProps() {
   const res = await fetch(url)
   const data = await res.json()
 
+  if(!data) {
+    return {
+      notFound: true,
+    }
+  }
+
   return {
     props: {
       data,
     },
+    revalidate: 10,
   }
 }
