ebb398299c59be7dd045a69fdf3634b2e2085929
Arian1192 <Arian.collaso.rodriguez@gmail.com>
2023-01-07 16:47:29 +0100

    change letter on mobile desing

--- components/Hero.js
+++ components/Hero.js
@@ -8,9 +8,9 @@ const Hero = ({ message, heading1, heading2, heading3 }) => {
     const ref = useRef(null)
     const isInView = useInView(ref, { once: true, threshold: 0.5 })
     return (
-        <div ref={ref} className='flex items-center justify-center h-screen mb-[10rem] bg-fixed bg-center bg-cover custom-img overflow-hidden '>
+        <div ref={ref} className='relative flex items-center justify-center h-screen mb-[10rem] bg-fixed bg-center bg-cover custom-img overflow-hidden'>
             {/* Overlay */}
-            <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
+            <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[1]' />
             <div className='p-5 text-white z-[2] flex flex-col items-center mt-[-8rem]'>
                 <h2 className='text-5xl font-bold tracking-wider max-sm:text-3xl' translate='no'
                 style={{
